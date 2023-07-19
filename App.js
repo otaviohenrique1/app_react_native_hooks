8cc84e7ab0e25c5386d9e322a694c4132603606f
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-07-19 15:52:34 -0300

    Commit 1

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, SafeAreaView } from 'react-native';
+import Home from './src/telas/Home';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Home/>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 16,
+  },
+});
+
+/*
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Cavalo.js</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,3 +38,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+*/
---
+++ src/telas/Home/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+import Topo from './components/Topo';
+
+export default function Home() {
+  return (
+    <Topo />
+  );
+}
